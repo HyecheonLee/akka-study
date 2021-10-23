8a1f5efe1f2a57085598587d61404abcf81c6ebe
이혜천 <rainbow880616@gmail.com>
2021-10-24 02:07:42 +0900

    FirstSimpleBehavior.kt

---
+++ src/main/kotlin/com/hyecheon/akkastudy/config/AkkaConfig.kt
@@ -0,0 +1,20 @@
+package com.hyecheon.akkastudy.config
+
+import akka.actor.typed.ActorSystem
+import com.hyecheon.akkastudy.behavior.FirstSimpleBehavior
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+/**
+ * User: hyecheon lee
+ * Email: rainbow880616@gmail.com
+ * Date: 2021/10/24
+ */
+@Configuration
+class AkkaConfig {
+
+    @Bean
+    fun firstActorSystem() = run {
+        ActorSystem.create(FirstSimpleBehavior.create(), "FirstActorSystem")
+    }
+}
