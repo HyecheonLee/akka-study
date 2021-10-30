cdcc2ac0836df22ab04fe452be2548a287428aee
이혜천 <rainbow880616@gmail.com>
2021-10-30 18:34:25 +0900

    코드 정리.

--- src/main/kotlin/com/hyecheon/akkastudy/behavior/WorkerBehavior.kt
+++ src/main/kotlin/com/hyecheon/akkastudy/behavior/WorkerBehavior.kt
@@ -4,7 +4,7 @@ import akka.actor.typed.ActorRef
 import akka.actor.typed.javadsl.AbstractBehavior
 import akka.actor.typed.javadsl.ActorContext
 import akka.actor.typed.javadsl.Behaviors
-import com.hyecheon.akkastudy.behavior.ManagerBehavior.*
+import akka.actor.typed.javadsl.Receive
 import com.hyecheon.akkastudy.behavior.ManagerBehavior.Command.Result
 import org.slf4j.LoggerFactory
 import java.io.Serializable
@@ -17,35 +17,46 @@ import java.util.*
  * Date: 2021/10/24
  */
 class WorkerBehavior(context: ActorContext<Command>) : AbstractBehavior<WorkerBehavior.Command>(context) {
-    private val log = LoggerFactory.getLogger(this::class.java)
+	private val log = LoggerFactory.getLogger(this::class.java)
 
-    companion object {
-        fun create() = Behaviors.setup(::WorkerBehavior)
-    }
+	companion object {
+		fun create() = Behaviors.setup(::WorkerBehavior)
+	}
 
-    sealed class Command : Serializable {
-        data class Start(val sender: ActorRef<ManagerBehavior.Command>? = null) : Command()
-    }
-
-    private var prime: BigInteger? = null
+	sealed class Command : Serializable {
+		data class Start(val sender: ActorRef<ManagerBehavior.Command>? = null) : Command()
+	}
 
 
-    override fun createReceive() = run {
-        newReceiveBuilder()
-            .onAnyMessage { receiveMsg ->
-                when (receiveMsg) {
-                    is Command.Start -> {
-                        if (prime == null) {
-                            val bigInteger = BigInteger(2000, Random())
-                            prime = bigInteger.nextProbablePrime()
-                        }
-                        prime?.let {
-                            receiveMsg.sender?.tell(Result(it))
-                        }
-                    }
-                }
-                this
-            }
-            .build()
-    }
+	override fun createReceive(): Receive<Command> {
+		return handlerMessagesWhenWeDontYetHaveAPrimeNumber()
+	}
+
+	fun handlerMessagesWhenWeDontYetHaveAPrimeNumber() = run {
+		newReceiveBuilder()
+			.onAnyMessage { receiveMsg ->
+				when (receiveMsg) {
+					is Command.Start -> {
+						val bigInteger = BigInteger(2000, Random())
+						val prime = bigInteger.nextProbablePrime()
+						handleMessageWhenWeAlreadyHaveAPrimeNumber(prime)
+					}
+				}
+			}
+			.build()
+	}
+
+	fun handleMessageWhenWeAlreadyHaveAPrimeNumber(prime: BigInteger) = run {
+		newReceiveBuilder()
+			.onAnyMessage { receiveMsg ->
+				when (receiveMsg) {
+					is Command.Start -> {
+						receiveMsg.sender?.tell(Result(prime))
+						Behaviors.same()
+					}
+				}
+			}
+			.build()
+	}
+
 }
--- src/test/kotlin/com/hyecheon/akkastudy/akka/ActorSystemTest.kt
+++ src/test/kotlin/com/hyecheon/akkastudy/akka/ActorSystemTest.kt
@@ -21,49 +21,49 @@ import java.lang.Thread.sleep
 @Import(AkkaConfig::class)
 class ActorSystemTest {
 
-    @Autowired
-    @Qualifier("firstActorSystem")
-    lateinit var actorSystem: ActorSystem<String>
+	@Autowired
+	@Qualifier("firstActorSystem")
+	lateinit var actorSystem: ActorSystem<String>
 
-    @Autowired
-    @Qualifier("workerActorSystem")
-    lateinit var workActorSystem: ActorSystem<WorkerBehavior.Command>
+	@Autowired
+	@Qualifier("workerActorSystem")
+	lateinit var workActorSystem: ActorSystem<WorkerBehavior.Command>
 
-    @DisplayName("1. tell")
-    @Test
-    internal fun test_1() {
-        actorSystem.tell("Hello are you there?")
-        actorSystem.tell("This is the second message.")
-    }
-
-
-    @DisplayName("2. 메시지 분리 테스트")
-    @Test
-    internal fun test_2() {
-        actorSystem.tell("say hello")
-        actorSystem.tell("who are you")
-        actorSystem.tell("This is the second message.")
-    }
-
-    @DisplayName("3. 자식 생성")
-    @Test
-    internal fun test_3() {
-        actorSystem.tell("create a child")
-    }
+	@DisplayName("1. tell")
+	@Test
+	internal fun test_1() {
+		actorSystem.tell("Hello are you there?")
+		actorSystem.tell("This is the second message.")
+	}
 
 
-    @DisplayName("4. worker ActorSystem")
-    @Test
-    internal fun test_4() {
-        workActorSystem.tell(WorkerBehavior.Command.Start())
-        sleep(1000)
-    }
+	@DisplayName("2. 메시지 분리 테스트")
+	@Test
+	internal fun test_2() {
+		actorSystem.tell("say hello")
+		actorSystem.tell("who are you")
+		actorSystem.tell("This is the second message.")
+	}
 
-    @DisplayName("5. manager ActorSystem")
-    @Test
-    internal fun test_5() {
-        val bigPrimes = ActorSystem.create(ManagerBehavior.create(), "BigPrime")
-        bigPrimes.tell(ManagerBehavior.Command.Start)
-        sleep(2000)
-    }
+	@DisplayName("3. 자식 생성")
+	@Test
+	internal fun test_3() {
+		actorSystem.tell("create a child")
+	}
+
+
+	@DisplayName("4. worker ActorSystem")
+	@Test
+	internal fun test_4() {
+		workActorSystem.tell(WorkerBehavior.Command.Start())
+		sleep(10000)
+	}
+
+	@DisplayName("5. manager ActorSystem")
+	@Test
+	internal fun test_5() {
+		val bigPrimes = ActorSystem.create(ManagerBehavior.create(), "BigPrime")
+		bigPrimes.tell(ManagerBehavior.Command.Start)
+		sleep(20000)
+	}
 }
